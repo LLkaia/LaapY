48b67169fb52283076e3d1acd9a5ccda66673d96
maksym-shulha <123635020+maksym-shulha@users.noreply.github.com>
2024-01-09 10:13:22 +0200

    add sequential proxying for laptops/id route with accesories and ratings. add lua script for processing ratings response

--- data/mongo/init-mongo.sh
+++ data/mongo/init-mongo.sh
@@ -8,4 +8,8 @@ mongoimport --host=localhost --port=27017 --db=lappy --collection=specs --file=/
     grep -E '(document\(s\) imported successfully|document\(s\) failed to import)' | \
     tail -n 1
 
+mongoimport --host=localhost --port=27017 --db=lappy --collection=ratings --file=/mongo/lappy_ratings.json 2>&1 | \
+    grep -E '(document\(s\) imported successfully|document\(s\) failed to import)' | \
+    tail -n 1
+
 tail -f /dev/null
