238c8ab88764ab4a7a5305d5fd55bfb5fc3c762e
DenysMkl <den.mikliaiev@gmail.com>
2024-01-15 00:17:14 +0200

    edit init-mongo.sh file

--- mongodb/init-mongo.sh
+++ mongodb/init-mongo.sh
@@ -14,15 +14,3 @@ for file in ${list_of_files[@]}; do
   name_without_ext=$(echo "$name" | cut -d'.' -f1)
   mongoimport --host=localhost --port=27017 --db=lappy --collection=$name_without_ext --file=/mongo/$name
 done
-
-#mongoimport --host=localhost --port=27017 --db=lappy --collection=specs --file=/mongo/lappy_specs.json
-#
-#mongoimport --host=localhost --port=27017 --db=lappy --collection=ratings --file=/mongo/lappy_ratings.json
-#
-#mongoimport --host=localhost --port=27017 --db=lappy --collection=search_results --file=/mongo/news.json
-#
-#mongoimport --host=localhost --port=27017 --db=lappy --collection=Cases --file=/mongo/Cases.json
-#
-#mongoimport --host=localhost --port=27017 --db=lappy --collection=Disks --file=/mongo/Disks.json
-#
-#mongoimport --host=localhost --port=27017 --db=lappy --collection=Headphones --file=/mongo/Headphones.json
